5a38e7d29ce5dfcfc63c15ea2bc31a368cbfe4bd
Viktor Bauer <bassa555@mail.ru>
2023-09-05 17:26:26 +0300

    homework middleware for review

---
+++ src/Book.js
@@ -0,0 +1,16 @@
+const { v4: uuid } = require('uuid');
+
+class Book {
+    constructor(title = '', authors = '', description = '', favorite = false, fileCover = '', fileName = '') {
+        this.title = title;
+        this.authors = authors;
+        this.description = description;
+        this.favorite = favorite;
+        this.fileCover = fileCover;
+        this.fileName = fileName;
+        this.fileBook = fileBook;
+        this.id = uuid();
+    }
+}
+
+module.exports = Book
---
+++ src/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('./middleware/logger');
+
+const error404 = require('./middleware/error-404');
+const userRouter = require('./routes/user');
+const booksRouter = require('./routes/books');
+
+const app = express();
+app.use(express.json);
+
+app.use(logger);
+app.use('/api/user', userRouter);
+app.use('/api/books', booksRouter);
+app.use(error404);
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT);
